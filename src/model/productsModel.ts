3fa3d4e474dff8c190ced66cc794da061ed1a89d
christian Pabon <christian75876@gmial.com>
2024-07-31 22:34:54 -0400

    add

---
+++ src/model/productsModel.ts
@@ -0,0 +1,26 @@
+import { pool } from "../config/db";
+
+export const findAll = async () => {
+    try {
+        const [products] = await pool.query("SELECT * FROM Products");
+        return products;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        throw new Error('Error fetching products: funtion: findAll');
+    }
+};
+
+export const findById = async (id: number) => {
+    try {
+        const [product] = await pool.query("SELECT * FROM Products WHERE id = ?", [id]);
+        return product;
+    } catch (error) {
+        console.error('Error fetching product:', error);
+        throw new Error('Error fetching product: function: findById');
+    }
+};
+
+export const save = async () => {
+
+};
+
